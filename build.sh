fafc2f8897250e024b74327f9d6c3b750a022dc0
yungsg <ysg@nus.edu.sg>
2022-04-10 21:43:33 +0800

    fix pkg deps, dockerfile

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -ex
 
+go mod edit -replace github.com/dgrijalva/jwt-go=github.com/dgrijalva/jwt-go/v4@v4.0.0-preview1
+go mod edit -replace github.com/gogo/protobuf=github.com/gogo/protobuf@v1.3.2
 go mod tidy
 go build -o ./traefik-forward-auth ./cmd/main.go
 ls -l ./traefik-forward-auth

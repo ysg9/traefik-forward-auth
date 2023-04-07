18f8d292f84339ed0eae0c3fd092f8bd8eef66ca
yungsg <ysg@nus.edu.sg>
2023-04-07 19:16:11 +0800

    add debug msgs

--- internal/handlers/server.go
+++ internal/handlers/server.go
@@ -330,6 +330,7 @@ func (s *Server) AuthCallbackHandler() http.HandlerFunc {
 			http.Error(w, "Bad Gateway", 502)
 			return
 		}
+		logger.Debugf("claims: %+v", claims)
 
 		email, ok := claims["email"]
 		if ok {
